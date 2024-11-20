0e6e206bf6ae58f63c99a5bf5b46f26c67f4c855
ehdwoKIM <kinbell19@gmail.com>
2024-11-21 00:45:09 +0900

    Docker 이미지 빌드 자동화(github action)

--- solmoi-express/src/app.js
+++ solmoi-express/src/app.js
@@ -2,9 +2,9 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res)=> {
-  res.status(200).send('Hello!');
-});
+// app.get('/', (req, res)=> {
+//   res.status(200).send('Hello!');
+// });
 
 app.listen(3000, () => {
   console.log('server is running');
