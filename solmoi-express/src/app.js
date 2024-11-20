6d1a766e5a91e001f49f48321021a4b81bf51d53
ehdwoKIM <kinbell19@gmail.com>
2024-11-21 01:56:55 +0900

    fix: ec2 배포 자동화

--- solmoi-express/src/app.js
+++ solmoi-express/src/app.js
@@ -6,6 +6,5 @@ app.get('/', (req, res)=> {
   res.status(200).send('Hello!');
 });
 
-app.listen(3000, () => {
-  console.log('server is running');
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
