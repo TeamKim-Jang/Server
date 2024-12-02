2deefe9da33911cc7c4189b073bd4c917e2a884f
akrxso <tjgus991008@gmail.com>
2024-12-02 10:04:05 +0900

    올리기전 준비

--- solmoi-express/src/models/User.js
+++ solmoi-express/src/models/User.js
@@ -1,15 +1,44 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../config/db.js';
+  import { DataTypes } from 'sequelize';
+  import sequelize from '../config/db.js';
 
-const User = sequelize.define('User', {
-  user_id: {
-    type: DataTypes.STRING,
-    primaryKey: true,
-  },
-  total_sol_leaf: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0,
-  },
-});
-
-export default User;
+  const User = sequelize.define(
+    'User', // 모델 이름
+    {
+      user_id: {
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      user_name: {
+        type: DataTypes.STRING(20),
+      },
+      nickname: {
+        type: DataTypes.STRING(20),
+      },
+      birth_date: {
+        type: DataTypes.STRING(200),
+      },
+      phone_number: {
+        type: DataTypes.BIGINT,
+      },
+      email: {
+        type: DataTypes.STRING(200),
+      },
+      password: {
+        type: DataTypes.STRING(200),
+      },
+      total_sol_leaf: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      school_id: {
+        type: DataTypes.BIGINT,
+      },
+    },
+    {
+      tableName: 'User', // 정확히 대문자로 시작하는 테이블 이름 지정
+      timestamps: false, // createdAt, updatedAt 자동 생성 방지
+    }
+  );
+  
+  export default User;
