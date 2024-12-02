ad9b8e993d2454b47f3bf087b62985c1ee8cf47f
ehdwoKIM <kinbell19@gmail.com>
2024-12-02 16:09:42 +0900

    fix: DB models 관계 수정

---
+++ solmoi-express/src/models/Prediction.js
@@ -0,0 +1,55 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+import Stock from "./Stock.js"; 
+
+const Prediction = sequelize.define(
+  "Prediction",
+  {
+    prediction_id: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    prediction_date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    prediction_upordown: {
+      type: DataTypes.ENUM("UP", "DOWN"),
+      allowNull: false,
+    },
+    is_correct: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true, 
+    },
+    user_id: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      references: {
+        model: "User",
+        key: "user_id",
+      },
+    },
+    stock_id: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      references: {
+        model: "Stock", 
+        key: "stock_id",
+      },
+    },    
+    prediction_stock_value: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: true,
+    },
+  },
+  {
+    tableName: "Prediction",
+    timestamps: false,
+  }
+);
+
+Prediction.belongsTo(Stock, { foreignKey: "stock_id" });
+
+export default Prediction;
