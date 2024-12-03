83cb4d091706435393ab98f0058de1e165de3344
J2Jayy <betatest0710@gmail.com>
2024-12-04 01:37:24 +0900

    add:auth

--- solmoi-express/src/config/db.js
+++ solmoi-express/src/config/db.js
@@ -5,11 +5,11 @@ dotenv.config();
 const { MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env;
 // Docker 환경에서는 host: "db"
 // 로컬 dev 환경에서는 host: "localhost"
-const sequelize = new Sequelize(MYSQL_DATABSE, MYSQL_USER, MYSQL_PASSWORD, {
+const sequelize = new Sequelize(MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD, {
   host: "localhost",
   port: MYSQL_PORT || 3306,
   dialect: "mysql",
   logging: false,
 });
-
+console.log(sequelize.config);
 export default sequelize;
