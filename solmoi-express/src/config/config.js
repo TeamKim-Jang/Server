0cef98f53d8582c7cfc0eeb5eb688161dffd1eb9
J2Jayy <betatest0710@gmail.com>
2024-12-04 00:01:06 +0900

    hotfix

--- solmoi-express/src/config/config.js
+++ solmoi-express/src/config/config.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  jwtSecret: "9166bb7804ba4d91b0022ba2ee234868",
+  jwtSecret: process.env.JWT_SECRET,
 };
 
 export default config;
--- solmoi-express/src/config/db.js
+++ solmoi-express/src/config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 const { MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env;
 // Docker 환경에서는 host: "db"
 // 로컬 dev 환경에서는 host: "localhost"
-const sequelize = new Sequelize("userDB", "root", "00000000", {
+const sequelize = new Sequelize(MYSQL_DATABSE, MYSQL_USER, MYSQL_PASSWORD, {
   host: "localhost",
   port: MYSQL_PORT || 3306,
   dialect: "mysql",
