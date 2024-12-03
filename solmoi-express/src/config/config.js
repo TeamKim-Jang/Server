db94ccf2543cef968a9461bf99c20e3537da751c
J2Jayy <betatest0710@gmail.com>
2024-12-03 19:03:16 +0900

    asdf

--- solmoi-express/src/config/config.js
+++ solmoi-express/src/config/config.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  jwtSecret: process.env.JWT_SECRET,
+  jwtSecret: "9166bb7804ba4d91b0022ba2ee234868",
 };
 
 export default config;
--- solmoi-express/src/controllers/authController.js
+++ solmoi-express/src/controllers/authController.js
@@ -1,10 +1,18 @@
 //controllers/authController.js
-import authService from '../services/authService.js';
+import authService from "../services/authService.js";
 
 const authController = {
   register: async (req, res) => {
     try {
-      const { user_name, nickname, birth_date, phone_number, email, password, school_name } = req.body;
+      const {
+        user_name,
+        nickname,
+        birth_date,
+        phone_number,
+        email,
+        password,
+        school_name,
+      } = req.body;
       console.log(req.body);
       // 회원가입 처리
       const result = await authService.registerUser(
@@ -25,26 +33,32 @@ const authController = {
         return res.status(400).json({ error: "이미 사용 중인 이메일입니다." });
       }
       // 기타 에러 처리
-      return res.status(500).json({ error: "회원가입 처리 중 문제가 발생했습니다." });
+      return res
+        .status(500)
+        .json({ error: "회원가입 처리 중 문제가 발생했습니다." });
     }
   },
 
   login: async (req, res) => {
     try {
       const { email, password } = req.body;
-  
+
       // 로그인 처리
       const result = await authService.loginUser(email, password);
       res.status(200).json(result); // 성공 응답
     } catch (error) {
       console.error("Login error:", error);
-  
+
       // 에러 메시지에 따라 적절한 상태 코드와 메시지 반환
       if (error.message === "Invalid email or password") {
-        return res.status(401).json({ error: "로그인 정보가 올바르지 않습니다." });
+        return res
+          .status(401)
+          .json({ error: "로그인 정보가 올바르지 않습니다." });
       }
       // 기타 예상치 못한 서버 에러 처리
-      return res.status(500).json({ error: "서버에서 문제가 발생했습니다. 다시 시도해주세요." });
+      return res
+        .status(500)
+        .json({ error: "서버에서 문제가 발생했습니다. 다시 시도해주세요." });
     }
   },
   logout: async (req, res) => {
@@ -54,10 +68,10 @@ const authController = {
       if (user) {
         await user.update({ access_token: null });
       }
-      res.status(200).json({ message: '로그아웃 성공' });
+      res.status(200).json({ message: "로그아웃 성공" });
     } catch (error) {
-      console.error('Logout error:', error.message);
-      res.status(500).json({ error: '로그아웃 처리 중 문제가 발생했습니다.' });
+      console.error("Logout error:", error.message);
+      res.status(500).json({ error: "로그아웃 처리 중 문제가 발생했습니다." });
     }
   },
 };
