f574c475f7b560838b009c53adb9a5a4a539e6bc
김동재 <87058086+ehdwoKIM@users.noreply.github.com>
2024-12-05 08:15:19 +0900

    Update rankingRepository.js

--- solmoi-express/src/repositories/rankingRepository.js
+++ solmoi-express/src/repositories/rankingRepository.js
@@ -1,5 +1,5 @@
 import Ranking from '../models/Ranking.js';
-import User from '../models/User.js';
+import User from '../models/user.js';
 import School from '../models/School.js';
 import Sequelize from 'sequelize';
 
