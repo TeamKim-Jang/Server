ee8568103a3f21458aff2f05301521b083a562a5
김동재 <87058086+ehdwoKIM@users.noreply.github.com>
2024-12-05 08:25:17 +0900

    Update db.js

--- solmoi-express/src/config/db.js
+++ solmoi-express/src/config/db.js
@@ -6,7 +6,7 @@ const { MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env;
 // Docker 환경에서는 host: "db"
 // 로컬 dev 환경에서는 host: "localhost"
 const sequelize = new Sequelize(MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD, {
-  host: "localhost",
+  host: "db",
   port: MYSQL_PORT || 3306,
   dialect: "mysql",
   logging: false,
